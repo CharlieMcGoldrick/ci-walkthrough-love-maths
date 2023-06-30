7b0a68cae05cf2ece3366f4eda566ecd8a3a046c
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-06-30 10:49:19 +0000

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -71,6 +73,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -102,8 +106,13 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
-
+/**
+* Displays a substraction question
+*/
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2; // Which is bigger: operand 1 or operand 2? If operand1 is bigger return that, else return operand 2
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1; // Which is bigger: operand 1 or operand 2? If operand1 is bigger return that, else return operand 2
+    document.getElementById('operator').textContent = '-';
 }
 
 /**
